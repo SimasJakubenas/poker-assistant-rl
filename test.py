fd3d8491a2730df5c47604d2cb77ed403fe752e5
sjakubenas <sjakubenas@gmail.com>
2025-03-17 04:22:17 +0200

    Undo previous change

--- test.py
+++
@@ -1,8 +0,0 @@
-import os
-import pandas as pd
-
-folder_path = f"outputs/datasets/dataframe/v02"
-if os.path.exists(folder_path): 
-    data = pd.read_csv(f"{folder_path}/eval.csv")
-    data['Total'] = data['3'].cumsum().round(2)
-    print(data)
